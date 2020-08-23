00b7381655915ca3ce62c7ffc0a10378c8747476
Jeffrey Clark <jmclark85@gmail.com>
2020-08-23 07:39:25 -0400

    Added processMains

---
+++ src/processMains.m
@@ -0,0 +1,18 @@
+function [scalar, time] = processMains(distance, sCoord, splay, p, c, isEnabled)
+%PROCESSMAINS Performs the conversions and processing for the main mic
+%array.
+%   Takes in the raw values for the main microphone array and returns the
+%   time and scalar values after any necessary conversion. Outputs
+%   appropriate values if pair is not enabled.
+
+if isEnabled == true
+    d = cm2M(distance);
+    vals = getMicPair(d, sCoord, play, p, c);
+else
+    vals = [1000 0];
+end
+
+
+
+end
+
