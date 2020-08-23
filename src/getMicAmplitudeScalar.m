4be605101e803631ef071d2bdfccc099b842d3f6
Jeffrey Clark <jmclark85@gmail.com>
2020-08-23 05:22:32 -0400

    Fixed off-axis angle calculation

--- src/getMicAmplitudeScalar.m
+++ src/getMicAmplitudeScalar.m
@@ -1,10 +1,10 @@
-function scalar = getMicAmplitudeScalar(mCoord, sCoord, splay, p)
+function scalar = getMicAmplitudeScalar(mCoord, sCoord, thetaM, p)
 %GETMICAMPLITUDESCALAR gets the amplitude scalar for a vMic
 %   This function takes in the coordinates [x,y] of a virtual microphone and
 %   the sound source along with a polar pattern and uses the
 %   virtualMicrophone and getThetaMS classes to calculate the amplitude scalar.
 
-theta = getThetaMS(mCoord, sCoord) + splay;
+theta = getThetaMS(mCoord, sCoord, thetaM);
 scalar = virtualMicrophone(p, theta);
 
 end
--- src/getThetaMS.m
+++ src/getThetaMS.m
@@ -1,9 +1,10 @@
 % This function finds how off-axis the source is from the microphone
 % (theta) by using the four-quadrant inverse tangent function and the
 % cartesian coordinate values for the virtual microphone and source's
-% positions on the virtual sound stage.
-function theta = getThetaMS(micPosXY, sourcePosXY)
-    x = micPosXY(1) - sourcePosXY(1);
-    y = micPosXY(2) - sourcePosXY(2);
-    theta = atan2(y, x);
+% positions on the virtual sound stage. The thetaM input should be in
+% radians.
+function thetaMS = getThetaMS(micPosXY, sourcePosXY, thetaM)
+    x = sourcePosXY(1) - micPosXY(1);
+    y = sourcePosXY(2) - micPosXY(2);
+    thetaMS = atan2(y, x) - thetaM;
 end
