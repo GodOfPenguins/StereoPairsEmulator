46fbe3201955b2448f7fe0960d7e62faa96288f7
Jeffrey Clark <jmclark85@gmail.com>
2020-08-25 03:01:04 -0400

    Update sourceCartesianCoordinates.m
    Added in the angular conversion.

--- src/sourceCartesianCoordinates.m
+++ src/sourceCartesianCoordinates.m
@@ -1,5 +1,11 @@
 function pos = sourceCartesianCoordinates(az, distance)
-    theta = az * (pi/180);
+% Converts the source angle and distance to cartesian coordinates.
+%   Takes in the angle from center in degrees and the distance in meters that the
+%   source is and converts it to x,y cartesian coordinates.
+    
+    % Convert the angular deflection into radians of rotation
+    theta = (az * (pi/180)) + (pi/2);
+    % Get Coordinates
     basis = [cos(theta), sin(theta)];
     pos = distance * basis;
 end
